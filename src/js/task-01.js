2d1c198e397ba8c3f412c95bd076bbf6c3dc95fd
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-03-19 20:02:00 +0100

    task-01 done

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const categories = document.querySelector("#categories");
+console.log(`Number of categories: ${categories.children.length}`);
+console.log("");
+const item = document.querySelectorAll(".item");
+const header = item.forEach((i) =>
+  console.log(
+    `Category: ${i.firstElementChild.textContent}`,
+    `Elements: ${i.lastElementChild.childElementCount}`
+  )
+);
