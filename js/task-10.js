4c01ee1448d4c4c069be71ed10c6b76fc90ed8cd
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-03-26 19:01:45 +0200

    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input[type = "number"]');
+const createEl = document.querySelector("button[data-create]");
+const destroyEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.getElementById("boxes");
+
+const createBoxes = () => {
+  for (let i = 0; i < inputEl.value; i++) {
+    let size = 30;
+    let div = document.createElement("div");
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.height = size + i * 10 + "px";
+    div.style.width = size + i * 10 + "px";
+    boxesEl.append(div);
+  }
+};
+const destroyBoxes = () => (boxesEl.innerHTML = "");
+
+createEl.addEventListener("click", createBoxes);
+destroyEl.addEventListener("click", destroyBoxes);
